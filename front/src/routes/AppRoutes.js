97f9c033550cc78b25b1e699e8cb992d4b06aea4
SouheirAlJammal <jammal.souheir@gmail.com>
2024-02-13 16:08:51 +0200

    setUP

---
+++ front/src/routes/AppRoutes.js
@@ -0,0 +1,18 @@
+import {Routes, Route} from 'react-router-dom'
+import React from 'react'
+import Login from '../components/Login/Login.js'
+import Signup from '../components/Signup/Signup.js'
+
+const AppRoutes = () => {
+  return (
+    <div>
+        <Routes>
+            <Route path='/login' element={<Login/>} />
+            <Route path='/signup' element={<Signup/>} />
+
+        </Routes>
+    </div>
+  )
+}
+
+export default AppRoutes
