25e8e022b291556e2404b81608c00948d45da78f
jammal.souheir@gmail.com <jammal.souheir@gmail.com>
2024-02-14 21:32:40 +0200

    motion background

--- front/src/routes/AppRoutes.js
+++ front/src/routes/AppRoutes.js
@@ -2,12 +2,12 @@ import { Routes, Route } from 'react-router-dom'
 import React from 'react'
 import Login from '../components/Login/Login.js'
 import Signup from '../components/Signup/Signup.js'
-
+import BackGround from '../components/BackGround/BackGround.js'
 const AppRoutes = () => {
   return (
     <div>
       <Routes>
-        <Route path='/' />
+        <Route path='/' element={<BackGround/>} />
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
         <Route path='/dashboard' />
