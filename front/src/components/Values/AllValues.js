6a1daacc04594c9c4162d2563f6566163b8fba4e
jammal.souheir@gmail.com <jammal.souheir@gmail.com>
2024-03-04 23:34:59 +0200

    editig responsive

--- front/src/components/Values/AllValues.js
+++ front/src/components/Values/AllValues.js
@@ -1,29 +1,60 @@
 // AllValues component
 import React from 'react';
-import prof from '../../assets/proficinant.png'
-import prioritize  from '../../assets/management.png'
-import flex from '../../assets/flexibiity.png'
+import graphs from '../../assets/graph.png'
 import style from './AllValues.module.css';
-import tasks from '../../assets/toDoTask.png'
+import stat from '../../assets/Man Pose - 03.png'
 import teamWork from '../../assets/team.png'
 import Value from './Value';
+import organised from '../../assets/Woman Pose - 03.png'
+import sharing from '../../assets/collaboration.png'
+import pencil from '../../assets/Lady with pencil.png'
 import { Element } from 'react-scroll';
 
 const AllValues = () => {
     const values = [
-        { srcImg: prioritize, alt: "alt", title: "Task Prioritization", content: "Easily prioritize your tasks based on importance and urgency, ensuring that you focus on what matters most." },
-        { srcImg: flex, alt: "alt", title: "Study Planning", content: "Create a study plan that fits your schedule and helps you stay on track with your learning goals." },
-        { srcImg: teamWork, alt: "alt", title: "Progress Tracking", content: "Track your daily progress and see how far you've come in your learning journey." },
-        { srcImg: prof, alt: "alt", title: "Efficiency Optimization", content: "Optimize your study sessions and tasks to make the most of your time and be more productive." },
-        { srcImg: tasks, alt: "alt", title: "Efficiency Optimization", content: "Optimize your study sessions and tasks to make the most of your time and be more productive." },
-        { srcImg: prof, alt: "alt", title: "Efficiency Optimization", content: "Optimize your study sessions and tasks to make the most of your time and be more productive." }
+        { 
+            srcImg: organised, 
+            alt: "alt", 
+            title: "Stay Organized", 
+            content: "Effortlessly organize your daily tasks and lessons, ensuring an efficient and structured approach to your continuous learning." 
+        },
+        { 
+            srcImg: sharing, 
+            alt: "alt", 
+            title: "Share Knowledge", 
+            content: "Connect with others, share your knowledge, and stay motivated within shared study plans." 
+        },
+        { 
+            srcImg: pencil, 
+            alt: "alt", 
+            title: "Continuous Learning", 
+            content: "Embrace a culture of continuous learning for personal and collective growth through ongoing education and skill development." 
+        },
+        { 
+            srcImg: teamWork, 
+            alt: "alt", 
+            title: "Collaborative Learning", 
+            content: "Create a collaborative learning environment by inviting friends to your study plans, fostering a shared journey towards knowledge and achievement." 
+        },
+        { 
+            srcImg: graphs, 
+            alt: "alt", 
+            title: "Check Progress", 
+            content: "Track your progress on each lesson and within the entire plan, comparing achievements and gaining insights into educational milestones." 
+        },
+        { 
+            srcImg: stat, 
+            alt: "alt", 
+            title: "Progress Visualization", 
+            content: "Effortlessly visualize progress on each lesson and the overall plan daily, weekly, and yearly for a comprehensive overview of your continuous learning journey." 
+        }
     ];
-
+    
     return (
         <Element className={style.valuesContainer} name="valuesSection" id='values'>
             <h2 className={style.title}>Values</h2>
             <p className={style.subtitle}>Stay organized and optimize your learning journey.</p>
-            <section className={style.values}>{values.map((elt,i)=> <Value value={elt} key={i} />)}</section>
+            <section className={style.values}>{values.map((elt, i) => <Value value={elt} key={i} />)}</section>
         </Element>
     );
 };
